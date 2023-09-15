2ec7e7ff13971fbb5bb55f0d894de19d748a3273
wasif415 <135797380+wasif415@users.noreply.github.com>
2023-09-15 10:07:07 +0500

    Add files via upload

---
+++ assemint.js
@@ -0,0 +1,70 @@
+// QUESTION NO 1 //
+
+// let fruits =["Apple","Banana","Orange"]
+// console.log(fruits)
+// fruits.push("Grapes")
+// console.log('fruits after push->',fruits)
+
+// QUESTION NO 2 //
+
+//let names =[]
+//console.log(names)
+//names.push("Alice", "Bob", "Charlie")
+//console.log('names after push->',names)
+
+// QUESTION NO 3 //
+
+// let numbers =[ 1,2,3,4,5 ]
+// console.log(numbers)
+// numbers.pop(5)
+// console.log('numbers after pop->',numbers)
+
+// QUESTION NO 4 //
+
+//let colors = ["red", "green", "blue"]
+//console.log(colors)
+//colors.unshift("yellow")
+//console.log('colors after unshift->',colors)
+
+// QUESTION NO 5 //
+
+//let days =["Monday", "Tuesday", "Wednesday", "Thursday"]
+//console.log(days)
+//days.shift("Monday")
+//console.log('days after shift->',days)
+
+// QUESTION NO 6 //
+
+//  let numbers = [10, 20, 30, 40, 50]; 
+//  let sum =0;
+//  for( let x =0; x < numbers.length; x++ ) {
+//   sum += number[x]
+//  }
+//  console.log("sum of numbers after->",sum);
+
+// let numbers = [10, 20, 30, 40, 50];
+// let sum = 0;
+// for (let x = 0; x < numbers.length; x++) {
+//    sum += numbers[x];
+//  }
+// console.log("Sum of numbers after->", sum);
+
+
+
+// QUESTION NO 7 //
+
+let evennumbers=[2,4,6,8,10,12,14,16,18,20]
+
+let 
+
+
+
+
+//QUESTION NO 8 //
+
+//let fruits=["apple", "banana", "orange", "kiwi"]
+//console.log(fruits)
+//for(let x=0;x< fruits.length;x++){
+//console.log(fruits[x])
+//}
+
